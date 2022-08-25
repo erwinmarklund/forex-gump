b2ad4aaa04ea157d5a979587610244f38176e5a7
12ström <jensen.m.tolfstrom@gmail.com>
2022-08-25 11:03:40 +0200

    Writing new empty methods
    Added test cases ;)

--- src/test/java/se/jensensthlm/forexgump/ExchangeCalculatorTest.java
+++ src/test/java/se/jensensthlm/forexgump/ExchangeCalculatorTest.java
@@ -11,6 +11,19 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ExchangeCalculatorTest {
+    @InjectMocks
+    ExchangeCalculator exchangeCalculator;
+
+    @Mock
+    ExchangeProvider exchangeProvider;
+
+    final double AMOUNT = 100.0;
+    final double RATE = 10.0;
+    final String REFERENCECURRENCY = "SEK";
+    final String TARGETCURRENCY = "GBP";
+
+
+
     @Test
     @DisplayName("Buy 100 GBP")
     public void TestBuy() {
@@ -20,10 +33,34 @@ public class ExchangeCalculatorTest {
     }
 
     @Test
+    @DisplayName("Buying same currency as reference throws exception")
+    public void TestBuyException(){
+
+    }
+
+    @Test
     @DisplayName("Sell 100 SEK to get GBP")
     public void TestSell() {
         // Given the exchange rate 10 SEK / GBP
         // Selling 100 SEK
         // Gives you 10 GBP
+    }
+
+    @Test
+    @DisplayName("Selling same currency as reference throws exception")
+    public void TestSellException(){
+
+    }
+
+    @Test
+    @DisplayName("Requesting exchange rate provides the correct rate for given currency pair")
+    public void TestGetExchangeRate(){
+
+    }
+
+    @Test
+    @DisplayName("Requesting exchange rate between the same currency throws an exception")
+    public void TestExchangeRateException(){
+
     }
 }
